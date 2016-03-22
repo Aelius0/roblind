d813ab96cac5205d03922e3980feacc43b5132b2
root <root@localhost.localdomain>
2016-03-22 18:17:11 +0100

    Update Kinect

--- src/Capteurs/Kinect.h
+++ src/Capteurs/Kinect.h
@@ -9,17 +9,34 @@
 
 //owner package header file
 
-
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui/highgui.hpp"
 
 class Kinect
 {
  public:
-void marquer_obstacles();
+    void marquer_obstacles();
+    void get_image(); //return 0 if ok else return 1
+
+//fonctions pour faire tourner la Kinect
+void modifyImage(int largeur, int longueur);
+void* freenect_threadfunc(void *arg);
+void rgb_cb(freenect_device *dev, void *rgb, uint32_t timestamp);
+void depth_cb(freenect_device *dev, void *v_depth, uint32_t timestamp);
+void* gl_threadfunc(void *arg);
+void InitGL(int Width, int Height);
+void ReSizeGLScene(int Width, int Height);
+void DrawGLScene();
 
  private:
-    void* profondeur;
-    void* image;
+    uint8_t* profondeur;
+    uint8_t* couleur;
+Mat mask;
 
+
+//trouver largeur et longueur des images: uint8** ?
+    
 }; //end class Kinect
 
 #endif // __Kinect_5c6b3e3f_fcd7_44fe_8410_f80405f349c8_H_INCLUDED
