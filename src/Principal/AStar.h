b13ded119b6751fdf855b7a472007267c5b6fb21
Matthieu Vigne <matthieu.vigne@mines-paristech.fr>
2016-03-22 11:31:13 +0100

    Algorithme A* avec Jump Point fonctionnel.

--- src/Principal/AStar.h
+++ src/Principal/AStar.h
@@ -1,4 +1,12 @@
 #ifndef ASTAR_H
 #define ASTAR_H
+	
+	//taille de la grille
+	#define n 200
+	#define m 200
+	#include <string>
 
+	std::string AStar(int startx, int starty, int endx, int endy); //algorithme A* : trouve un chemin de start a end, sur la carte map
+	int map[n][m]; //la carte utilisee par AStar
+	
 #endif // ASTAR_H
