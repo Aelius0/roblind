f058f2abab8d0c0f21981e837d3b81c14ea4990f
RodolpheDubois <rodolphe.dubois@mines-paristech.fr>
2016-03-26 15:27:24 +0100

    Update Robot.h

--- src/Principal/Robot.h
+++ src/Principal/Robot.h
@@ -33,31 +33,52 @@ using namespace std;
 class Robot
 {
  public:
-    bool en_evitement;
+    
+    /*COMPOSANTS DE LA CANNE*/
     Canne::Joystick* joystick;
     Canne::Bouton_arret* bouton_arret;
     Canne::Capteur_effort* capteur_effort;
     Canne::Capteur_presence* capteur_presence;
     Canne::Buzzer* buzzer;
     Canne::Vibreur* vibreur;
+    
+    /*CAPTEURS DE LA BASE MOBILE*/
     Kinect* kinect;
     GPS* gps;
     Batterie* batterie;
     Laser* laser_gauche;
     Laser* laser_droit;
-    Bouton_arret_urgence* bouton_arret_urgence;
-    Bouton_mode_guidage* bouton_mode_guidage;
-    double x;
-    double y;
-    double o; //orientation
-    int* chemin_evitement[];
-    // 0: vide  1: obstacle  2: trottoir  3: mur
-    Grille* grille_espace;
-	
     double hauteurLaser = 10; // TODO : fix
     double hauteurKinect = 15; // TODO : fix
-
-    void avancerRobot(int dist);
+    
+    /*BOUTONS D'ARRET ET DE SELECTION DE MODE*/
+    Bouton_arret_urgence* bouton_arret_urgence;
+    Bouton_mode_guidage* bouton_mode_guidage;
+    bool en_evitement;
+    
+    /*ATTRIBUTS DE POSITION ET DE DEPLACEMENT*/
+    double x; // abscisse
+    double y; // ordonnee
+    double o; //orientation
+    int caseX; // abscisse de la case correspondante dans la grille AJOUTE
+    int caseY; // ordonnee de la case correspondante dans la grille AJOUTE
+    // 0: vide  1: obstacle  2: trottoir  3: mur
+    Grille* grille_espace; // grille des obstacles
+    
+    /*PARAMETRES LIES A LA TRANSLATION DE LA GRILLE*/
+    double bordureSortie; // bordure a partir de laquelle translater la grille AJOUTE
+    double bordureEntree; // bordure de reapparition du robot apres translation AJOUTE
+    int caseInitialeXY; // abscisse et ordonnee de la case de reapparition AJOUTE
+    double positionInitialeXY; // abscisse et ordonnee initiale du robot AJOUTE
+    
+    /*TRAJECTOIRE CALCULEE*/
+    int* chemin_evitement[];
+ 
+	
+    /*METHODES DE DEPLACEMENT*/
+    void avancerRobot(double); // avec une distance absolue AJOUTE
+    void avancerRobot(int dist); // avec une distance en nombre de cases
+    void translaterGrille(); // translate la grille quand le robot atteint la bordure de sortie AJOUTE
     void main();
     bool calculer_chemin(double direction);
 
