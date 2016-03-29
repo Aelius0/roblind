6170e3497e404c2b926e7d10bdcc80589cef1486
RodolpheDubois <rodolphe.dubois@mines-paristech.fr>
2016-03-29 17:37:16 +0200

    Update ThreadMoteur.h

--- src/Moteurs/ThreadMoteur.h
+++ src/Moteurs/ThreadMoteur.h
@@ -9,10 +9,11 @@ int* position = new int;
 int lastCmd = -1;
 int lastCmdType = -1;
 int err = 0;
-bool active = 0;
 mode m;
 extern CKangaroo baseRoulante;
 void *ThreadMoteur (void *); //thread de lecture et d'ecriture de la carte de commande des moteurs
+
+extern bool active = 0;
 
 
 #endif // __Moteurs_8e1bc01c_abbd_488d_9630_5c9978d629bb_H_INCLUDED
