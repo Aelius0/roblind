ad5925d9bf90634e3eeeb44533ab1fd99aaed885
RodolpheDubois <rodolphe.dubois@mines-paristech.fr>
2016-04-17 10:57:55 +0200

    Update Robot.cpp

--- src/Principal/Robot.cpp
+++ src/Principal/Robot.cpp
@@ -13,35 +13,35 @@ struct coordonneesHorsGrille{};
 struct coordonneesHorsBordure{};
 
 /*CONSTRUCTEUR ET DESTRUCTEUR*/
-Robot::Robot(double resolution = RESOLUTION, int taille = DIMENSION){
+Robot::Robot(int taille = DIMENSION, double resolution = RESOLUTION){
 
 	cout << "----------------------\n";
 	cout << "CONSTRUCTION DU ROBOT.\n";
 
 	/*CREATION DE LA GRILLE*/
-	grille = new Grille(resolution,taille); // sert à repérer les obstacles
+	grille = new Grille(taille, resolution); // sert à repérer les obstacles
 
     	m_canne = new CCanne();
   	m_baseRoulante= new Cbaseroulante("/dev/ttyAMA0",27);
     	m_lidarLite = new lidarLite();
 
 	/*INITIALISATION DU PLACEMENT DU ROBOT*/
-	// On place le robot au milieu de la grille.
-	X=taille*resolution/2;
-	caseX=floor(x/resolution);
 
-	Y=taille*resolution/2;
-	caseY=floor(y/resolution);
+	x=grille->getDimension()/2;
+	y=grille->getDimension()/2;
 
-	positionInitialeXY = taille*resolution/2;
-	caseInitialeXY = floor(taille/2);
-
+	positionInitialeXY = grille->getDimension()/2;
+	//cout << "positionInitialeXY :" << positionInitialeXY << "\n";
+	caseInitialeXY = floor(grille->getDimension()/2);
+	//cout << "CaseInitialeXY : " << caseInitialeXY << "\n";
+	
 	o=0;
-	vitesse=0;
+	v=0;
+	en_evitement = true;
 
 	/*DEFINITON DES BORDURES D'ENTREE ET DE SORTIE*/
-	bordureEntree = taille*resolution/5; // A REDEFINIR
-	bordureSortie = taille*resolution/10; // A REDEFINIR
+	bordureEntree = grille->getDimension()/5; // A REDEFINIR
+	bordureSortie = grille->getDimension()/10; // A REDEFINIR
 
 	cout << "Fin de la construction du robot.\n";
 
@@ -99,102 +99,106 @@ void Robot::avancerRobot(double distance){
 	try{
 
 	/*CALCUL DE LA NOUVELLE POSITION*/
-	double deltaX = - math.abs(distance) * math.sin(o);
+	double deltaX = - abs(distance) * sin(o) / grille->getResolution();
 	x+=deltaX;
 
-	double deltaY = + math.abs(distance) * math.cos(o);
+	double deltaY = + abs(distance) * cos(o) / grille->getResolution();
 	y+=deltaY;
 
-	/*ACTUALISATION DE LA CASE*/
-	caseX=math.floor(x/(grille->getResolution()));
-	caseY=math.floor(y/(grille->getResolution()));
-
 	/*VERIFICATION DE LA NOUVELLE POSITION*/
-	double tailleGrille = (grille->getTaille())*(grille->getResolution());
-	if(x<0 || x>tailleGrille){throw coordonneesHorsGrille();}
-	else if(y<0 || y>tailleGrille){throw coordonneesHorsGrille();}
+	if(x<0 || x>grille->getDimension()){throw coordonneesHorsGrille();}
+	else if(y<0 || y>grille->getDimension()){throw coordonneesHorsGrille();}
 
-	if(x<bordureSortie || x>tailleGrille-bordureSortie){throw coordonneesHorsBordure();}
-	else if(y<bordureSortie || y>tailleGrille-bordureSortie){throw coordonneesHorsBordure();}
+	if(x<bordureSortie || x>grille->getDimension()-bordureSortie){throw coordonneesHorsBordure();}
+	else if(y<bordureSortie || y>grille->getDimension()-bordureSortie){throw coordonneesHorsBordure();}
 
 	}
-	
+
 	/*GESTION DES EXCEPTIONS*/
 	catch(coordonneesHorsGrille& e){
-		cout << "On est sorti de la grille. C'est pas bien. \n";
+		translaterGrille();
+		cout << "Sortie de la grille. \n";
 	}
 	catch(coordonneesHorsBordure& e){
-		cout << "Il va falloir translater la grille. APPELER LA BONNE METHODE.\n";
+		translaterGrille();
+		cout << "Sortie des bordures.\n";
+		cout << "On a translate la grille.\n";
 	}
-    
+
 }
 
 void Robot::actualiserLaser(double distMesuree, double inclinaison){
 
 	/*DISTANCE THEORIQUE ATTENDUE*/
-	double distTheorique = hauteurLaser / math.cos(inclinaison) ;
+	double distTheorique = hauteurLaser / cos(inclinaison) ;
 	// L'inclinaison se mesure par rapport à la normale verticale
 	// En regadant vers l'avant du robot, l'inclinaison est positive
 	// quand on se penche vers la droite
 
 	/*REPERAGE DE LA DISTANCE LATERALE DE L'OBSTACLE*/
-	double distObstacle = distMesuree * math.sin(inclinaison) ;
+	double distObstacle = distMesuree * sin(inclinaison) ;
 	// Distance positive si l'obstacle est à droite
 	// Distance négative si l'obstacle est à gauche
 
-	/*REPERAGE DE LA POSITION ET DE LA CASE*/
-	double Xobstacle = x + distObstacle * math.cos(o) ;
-	double Yobstacle = y + distObstacle * math.sin(o) ;
-
-	int caseObstacleX = floor(Xobstacle/grille->getResolution());
-	int caseObstacleY = floor(Yobstacle/grille->getResolution());
+	/*REPERAGE DE LA POSITION*/
+	double Xobstacle = x + (distObstacle * cos(o) / grille->getResolution() ) ;
+	double Yobstacle = y + (distObstacle * sin(o) / grille->getResolution() ) ;
 
 	/*MODIFICATION DE LA GRILLE*/
 	double deltaDist = distMesuree - distTheorique ;
 	double margeTolerance = 0.1*distTheorique; // Marge de tolerance de 10% de la distance theorique
 	if(deltaDist>=margeTolerance){
-		grille->set(caseObstacleX,caseObstacleY,CHAUSSEE);
+		grille->set(floor(Xobstacle),floor(Yobstacle),CHAUSSEE);
 	}
 	else if(abs(deltaDist)<margeTolerance){
-		grille->set(caseObstacleX,caseObstacleY,RAS);
+		grille->set(floor(Xobstacle),floor(Yobstacle),RAS);
 	}
 	else{
-		grille->set(caseObstacleX,caseObstacleY,OBSTACLE);
+		grille->set(floor(Xobstacle),floor(Yobstacle),OBSTACLE);
 	}
 }
 
 void Robot::translaterGrille(){
 
-	cout << "-------------------------------------\n";
 	cout << "TRANSLATION DE LA GRILLE\n";
 
 	/*CALCUL DES COORDONNEES D'ENTREE DESIREES*/
 	double deltaX = x - positionInitialeXY;
 	double deltaY = y - positionInitialeXY;
+	cout << "Le deltaX vaut : " << deltaX << "\n";
+	cout << "Le deltaY vaut : " << deltaY << "\n";
 
-	double numerateurRapport = (grille->getTailleGrille()/2)-bordureEntree;
-	double denominateurRapport = max(math.abs(deltaX),math.abs(deltaY));
+	double numerateurRapport = (grille->getDimension()/2)-bordureEntree;
+	cout << "Le numerateur du rapport : " << numerateurRapport << "\n";
+	double denominateurRapport = max(abs(deltaX),abs(deltaY));
+	cout << "Le denominateur du rapport : " << denominateurRapport << "\n";
 	double rapportHomothetie = numerateurRapport/denominateurRapport;
+	cout << "Le rapport d'homothetie : " << rapportHomothetie << "\n";
 	// La position de respawn est sur la bordure d'entree, inclue dans la bordure de sortie
 	// Sur la carre definie par la position de sortie, on determine la position symetrique par rapport au centre
 	// On se ramene sur la bordure d'entree avec le rapport d'homothetie entre les deux bordures
 	// Cette homothetie a pour centre le centre de la grille
 
 	double Xentree = positionInitialeXY - deltaX*rapportHomothetie;
-	int caseXentree = math.floor(Xentree/grille->getResolution());
+	cout << "Xentree : " << Xentree << "\n";
+	int caseXentree = floor(Xentree);
+	cout << "CaseXEntree : " << caseXentree << "\n";
 	double Yentree = positionInitialeXY - deltaY*rapportHomothetie;
-	int caseYentree = math.floor(Yentree/grille->getResolution());
+	cout << "Yentree : " << Yentree << "\n";
+	int caseYentree = floor(Yentree);
+	cout << "CaseYEntree : " << caseYentree << "\n";
 
 	/*DETERMINATION DES VECTEURS DE TRANSLATION ET TRANSLATION*/
-	int translationCaseX = caseXentree - caseX;
-	int translationCaseY = caseYentree - caseY;
+	int translationCaseX = caseXentree - floor(x);
+	int translationCaseY = caseYentree - floor(y);
+	cout << "Vecteur de translation sur x : " << translationCaseX << "\n";
+	cout << "Vecteur de translation sur y : " << translationCaseY << "\n";
 	grille->translaterRepereGrille(translationCaseX,translationCaseY);
 
 	/*ACTUALISATION DES POSITIONS DU ROBOT*/
 	x = Xentree;
-	caseX = caseXentree;
 	y = Yentree;
-	caseY = caseYentree;
+	cout << "La nouvelle position du robot : [" << x << "," << y << "]\n";
 
 	cout << "-------------------------------------\n";
 
@@ -245,7 +249,40 @@ double Robot::getDirection(){
 	
 }
 
+bool Robot::obstacleDevant(int dist)
+{
+    int dx = static_cast<int>(cos(o)*dist);
+    int dy = static_cast<int>(sin(o)*dist);
+    if (o < 3.14/*math.pi*/) {
+        if (o < 3.14/*math.pi*//2 || o > (3/2)*3.14/*math.pi*/) {
+            for (int i = 0; i < dx; ++i)
+                for (int j = 0; j < dy; ++j)
+                    if (grille->get(x + i, y + j) > 0)
+                        return true;
+        }
+        else {
+            for (int i = dx; i < 0; ++i)
+                for (int j = 0; j < dy; ++j)
+                    if (grille->get(x + i, y + j) > 0)
+                        return true;
+        }
+    } else {
+        for (int j = 0; j < dy; ++j)
+            if (o < 3.14/*math.pi*//2 || o > (3/2)*3.14/*math.pi*/) {
+                for (int i = 0; i < dx; ++i)
+                    if (grille->get(x + i, y + j) > 0)
+                        return true;
+            } else {
+                for (int i = dx; i < 0; ++i)
+                    if (grille->get(x + i, y + j) > 0)
+                        return true;
+            }
+    }
 
+    return true;
+}
+
+/*
 bool Robot::obstacleDevant(int dist = 10)
 {
     double angle = robot.o * 2 * math.pi / 360;
@@ -277,7 +314,7 @@ bool Robot::obstacleDevant(int dist = 10)
             }
     }
 }
-
+*/
 bool Robot::stop() {
     baseRoulante.stop();
 	delete path;
